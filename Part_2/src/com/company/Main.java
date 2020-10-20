adfbbdf5dc4cc869c0ca7ecc942d91cc337e09e5
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-10-20 20:45:32 +0200

    input() method is implemented in Tree

---
+++ Part_2/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Tree (root field with Node Object) class
+        // Node (value, leftChild, rightChild) - private class inside Tree class
+        // insert(value)
+        // find(value):boolean
+
+        // var current = root;
+        var tree=new Tree();
+        tree.insert(7);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(10);
+        System.out.println("Done!");
+    }
+}
