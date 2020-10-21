2a02e8a0ff36d8e1250268de74b4e00ce64316d3
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-10-22 01:41:38 +0200

    Equality checking method is implemented using recursion

--- Part_2/src/com/company/Main.java
+++ Part_2/src/com/company/Main.java
@@ -17,7 +17,15 @@ public class Main {
         tree.insert(6);
         tree.insert(8);
         tree.insert(10);
-        System.out.println(tree.height());
-        System.out.println(tree.min());
+
+        var tree2=new Tree();
+        tree2.insert(7);
+        tree2.insert(4);
+        tree2.insert(9);
+        tree2.insert(1);
+        tree2.insert(6);
+        tree2.insert(8);
+        tree2.insert(10);
+        System.out.println(tree.equals(tree2));
     }
 }
