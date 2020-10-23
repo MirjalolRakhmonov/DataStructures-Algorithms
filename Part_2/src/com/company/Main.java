6a5a65cc315795c7c3667c1c2acd068957e38c2d
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-10-23 13:32:32 +0200

    Exercise file is uploaded

--- Part_2/src/com/company/Main.java
+++ Part_2/src/com/company/Main.java
@@ -3,12 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // Tree (root field with Node Object) class
-        // Node (value, leftChild, rightChild) - private class inside Tree class
-        // insert(value)
-        // find(value):boolean
-
-        // var current = root;
         var tree=new Tree();
         tree.insert(7);
         tree.insert(4);
--- Part_2/src/com/company/Tree.java
+++ Part_2/src/com/company/Tree.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Tree {
     private class Node {
@@ -8,37 +9,36 @@ public class Tree {
         private Node leftChild;
         private Node rightChild;
 
-        public Node(int value){
-            this.value=value;
+        public Node(int value) {
+            this.value = value;
         }
 
         @Override
-        public String toString(){
+        public String toString() {
             return "Node=" + value;
         }
     }
 
     private Node root;
 
-    public void insert(int value){
-        var node=new Node(value);
-        if (root==null){
-            root=node;
+    public void insert(int value) {
+        var node = new Node(value);
+        if (root == null) {
+            root = node;
             return;
         }
 
-        var current=root;
-        while (true){
-            if (value<current.value) {
-                if (current.leftChild==null){  // that means we found a parent
-                    current.leftChild=node;
+        var current = root;
+        while (true) {
+            if (value < current.value) {
+                if (current.leftChild == null) {  // that means we found a parent
+                    current.leftChild = node;
                     break;
                 }
                 current = current.leftChild;
-            }
-            else {
-                if (current.rightChild==null){
-                    current.rightChild=node;
+            } else {
+                if (current.rightChild == null) {
+                    current.rightChild = node;
                     break;
                 }
                 current = current.rightChild;
@@ -46,26 +46,26 @@ public class Tree {
         }
     }
 
-    public boolean find(int value){
-        var current=root;
-        while (current!=null){
-            if (value<current.value)
-                current=current.leftChild;
-            else if (value>current.value)
-                current=current.rightChild;
+    public boolean find(int value) {
+        var current = root;
+        while (current != null) {
+            if (value < current.value)
+                current = current.leftChild;
+            else if (value > current.value)
+                current = current.rightChild;
             else
                 return true;
         }
         return false;
     }
 
-    public void traversePreOrder(){
+    public void traversePreOrder() {
         traversePreOrder(root);
     }
 
-    private void traversePreOrder(Node root){
+    private void traversePreOrder(Node root) {
         // Base condition, a moment we should stop going forward, we should go back
-        if (root==null)
+        if (root == null)
             return;
 
         System.out.println(root.value);
@@ -73,60 +73,60 @@ public class Tree {
         traversePreOrder(root.rightChild);
     }
 
-    public int height(){
+    public int height() {
         return height(root);
     }
 
-    private int height(Node root){
-        if (root==null)
+    private int height(Node root) {
+        if (root == null)
             return -1;
 
-        if (root.leftChild==null && root.rightChild==null)
+        if (root.leftChild == null && root.rightChild == null)
             return 0;
 
         return 1 + Math.max(height(root.leftChild), height(root.rightChild));
     }
 
     // O(log(n))
-    public int min(){
-        if (root==null)
+    public int min() {
+        if (root == null)
             throw new IllegalStateException();
 
-        var current=root;
-        var last=current;
-        while (current!=null){
-            last=current;
-            current=current.leftChild;
+        var current = root;
+        var last = current;
+        while (current != null) {
+            last = current;
+            current = current.leftChild;
         }
         return last.value;
     }
 
     // O(n) because we have to traverse every single node
-    private int min(Node root){
-        if (root.leftChild==null && root.rightChild==null)
+    private int min(Node root) {
+        if (root.leftChild == null && root.rightChild == null)
             return root.value;
 
-        var left=min(root.leftChild);
-        var right=min(root.rightChild);
+        var left = min(root.leftChild);
+        var right = min(root.rightChild);
 
-        return Math.min(Math.min(left,right), root.value);
+        return Math.min(Math.min(left, right), root.value);
     }
 
     public boolean equals(Tree other) {
-        if (other==null)
+        if (other == null)
             return false;
-        
+
         return equals(root, other.root);
     }
 
     private boolean equals(Node first, Node second) {
-        if (first==null && second==null)
+        if (first == null && second == null)
             return true;
 
-        if (first!=null && second!=null)
-            return first.value==second.value
-            && equals(first.leftChild, second.leftChild)
-            && equals(first.rightChild, second.rightChild);
+        if (first != null && second != null)
+            return first.value == second.value
+                    && equals(first.leftChild, second.leftChild)
+                    && equals(first.rightChild, second.rightChild);
 
         return false;
     }
@@ -136,37 +136,37 @@ public class Tree {
     }
 
     private boolean isBinarySearchTree(Node root, int min, int max) {
-        if (root==null)
+        if (root == null)
             return true;
 
-        if (root.value<min || root.value>max)
+        if (root.value < min || root.value > max)
             return false;
 
-        return isBinarySearchTree(root.leftChild, min, root.value-1)
-                && isBinarySearchTree(root.rightChild, root.value+1, max);
+        return isBinarySearchTree(root.leftChild, min, root.value - 1)
+                && isBinarySearchTree(root.rightChild, root.value + 1, max);
     }
 
     public ArrayList<Integer> getNodesAtDistance(int distance) {
-        var list =new ArrayList<Integer>();
+        var list = new ArrayList<Integer>();
         getNodesAtDistance(root, distance, list);
         return list;
     }
 
-    private void getNodesAtDistance(Node root, int distance, ArrayList<Integer> list ) {
-        if (root==null)
+    private void getNodesAtDistance(Node root, int distance, ArrayList<Integer> list) {
+        if (root == null)
             return;
 
-        if (distance==0){
+        if (distance == 0) {
             list.add(root.value);
             return;
         }
 
-        getNodesAtDistance(root.leftChild, distance-1, list);
-        getNodesAtDistance(root.rightChild, distance-1, list);
+        getNodesAtDistance(root.leftChild, distance - 1, list);
+        getNodesAtDistance(root.rightChild, distance - 1, list);
     }
 
     public void traverseLevelOrder() {
-        for (int i = 0; i<=height(); i++) {
+        for (int i = 0; i <= height(); i++) {
             for (var value : getNodesAtDistance(i))
                 System.out.println(value);
         }
@@ -177,7 +177,7 @@ public class Tree {
     }
 
     private int size(Node node) {
-        if (node==null)
+        if (node == null)
             return 0;
 
         return (size(node.leftChild) + 1 + size(node.rightChild));
@@ -188,36 +188,36 @@ public class Tree {
     }
 
     private int countLeaves(Node node) {
-        if (node==null)
+        if (node == null)
             return 0;
 
-        if (node.leftChild==null && node.rightChild==null)
+        if (node.leftChild == null && node.rightChild == null)
             return 1;
 
-        return countLeaves(node.leftChild)+countLeaves(node.rightChild);
+        return countLeaves(node.leftChild) + countLeaves(node.rightChild);
     }
 
     public int max() {
-        if (root==null)
+        if (root == null)
             throw new IllegalStateException();
 
-        var current=root;
-        var first=current;
-        while (current!=null) {
-            first=current;
-            current=current.rightChild;
+        var current = root;
+        var first = current;
+        while (current != null) {
+            first = current;
+            current = current.rightChild;
         }
         return first.value;
     }
 
     private int max(Node node) {
-        if (node.leftChild==null && node.rightChild==null)
+        if (node.leftChild == null && node.rightChild == null)
             return node.value;
 
-        var left=max(node.leftChild);
-        var right=max(node.rightChild);
+        var left = max(node.leftChild);
+        var right = max(node.rightChild);
 
-        return Math.max(Math.max(left,right), node.value);
+        return Math.max(Math.max(left, right), node.value);
     }
 
     public boolean contains(int value) {
@@ -225,15 +225,16 @@ public class Tree {
     }
 
     private boolean contains(Node node, int value) {
-        var current=root;
-        while (current!=null) {
-            if (value<current.value)
-                current=current.leftChild;
-            else if (value>current.value)
-                current=current.rightChild;
+        var current = root;
+        while (current != null) {
+            if (value < current.value)
+                current = current.leftChild;
+            else if (value > current.value)
+                current = current.rightChild;
             else
                 return true;
         }
         return false;
     }
+
 }
