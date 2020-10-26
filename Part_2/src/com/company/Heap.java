bd5a84fb4b3afa64419dfeb8d816a6a0385e6abc
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-10-26 21:31:46 +0100

    insert method is refactored

--- Part_2/src/com/company/Heap.java
+++ Part_2/src/com/company/Heap.java
@@ -5,16 +5,23 @@ public class Heap {
     private int size;
 
     public void insert(int value) {
-        if (size==items.length)
+        if (isFull())
             throw new IllegalStateException();
         items[size++]=value; // this is how we store item in next available slot
 
-        // Here below, we need bubbling up algorithm
+        bubbleUp();
+    }
+
+    private void bubbleUp() {
         var index=size-1;
         while (index>0 && items[index] > items[parent(index)]) {
             swap(index, parent(index));
             index=parent(index);
         }
+    }
+
+    public boolean isFull() {
+        return size==items.length;
     }
 
     private int parent(int index) {
