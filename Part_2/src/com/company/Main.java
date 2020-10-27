5716e929e98279309ed6dfa80915a466de6ce995
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-10-27 13:08:12 +0100

    Heapify algorithm is implemented

--- Part_2/src/com/company/Main.java
+++ Part_2/src/com/company/Main.java
@@ -5,12 +5,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] numbers={5, 3, 10, 1, 4, 2};
-        var heap=new Heap();
-        for (var number : numbers)
-            heap.insert(number);
-        for (var i = numbers.length-1; i>=0; i--)
-            numbers[i]=heap.remove();
+        int[] numbers={5, 3, 8, 4, 1, 2};
+        MaxHeap.heapify(numbers);
         System.out.println(Arrays.toString(numbers));
     }
 }
